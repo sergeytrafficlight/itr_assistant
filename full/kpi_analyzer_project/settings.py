f22bcb8d185490f38522ca0cd5947c05cd486464
Nick <drippyslaughter@gmail.com>
2025-12-09 12:34:42 +0300

    Update settings.py

--- full/kpi_analyzer_project/settings.py
+++ full/kpi_analyzer_project/settings.py
@@ -82,18 +82,7 @@ DATABASES = {
             'charset': 'utf8mb4',
         },
     },
-    'itrade': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'itrade',
-        'USER': 'itr_assistant',
-        'PASSWORD': 'D4Ht3Ry3i7VzjgcJ',
-        'HOST': '2.58.67.166',
-        'PORT': '3308',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
-        'CONN_MAX_AGE': 60,
+
     }
 }
 
